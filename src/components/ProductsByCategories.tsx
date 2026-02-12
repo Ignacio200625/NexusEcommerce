6b4adfb42c7eaf81292169bef72ccdfc8b856ee0
Nacho <alumno.770209@ies-azarquiel.es>
2026-02-12 13:20:03 +0100

    NoTrending

--- src/components/ProductsByCategories.tsx
+++ src/components/ProductsByCategories.tsx
@@ -55,9 +55,7 @@ function ProductsByCategories({products}:Props){
                 ${product.price.toFixed(2)}
               </span>
 
-              <span className="rounded-full bg-blue-50 px-3 py-1 text-sm font-medium text-[#0d7ff2]">
-                Trending
-              </span>
+             
             </div>
           </div>
 

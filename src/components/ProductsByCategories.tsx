7c8bab38cea959b0164192ab872178e03d22e61b
Nacho <alumno.770209@ies-azarquiel.es>
2026-02-12 13:19:18 +0100

    paddingProductSCategories

--- src/components/ProductsByCategories.tsx
+++ src/components/ProductsByCategories.tsx
@@ -8,7 +8,7 @@ interface Props{
 function ProductsByCategories({products}:Props){
 
     return(
-        <div className="grid grid-cols-2 px-10 md:px-40 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 px-5 md:px-40 sm:grid-cols-2 lg:grid-cols-4 gap-6">
          {products.map((product:Product) => (
         <motion.div
           key={product.id}
